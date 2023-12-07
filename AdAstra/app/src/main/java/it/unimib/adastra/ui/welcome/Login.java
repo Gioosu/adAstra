7e6a606732a5c825c35fb865f69fff95d74cea48
Riccardo Pretali <r.pretali@campus.unimib.it>
2023-12-07 13:18:39 +0100

    v0.06
    Implementazione del salvataggio crittografato dei dati di login (e-mail, password).

--- AdAstra/app/src/main/java/it/unimib/adastra/ui/welcome/Login.java
+++ AdAstra/app/src/main/java/it/unimib/adastra/ui/welcome/Login.java
@@ -1,5 +1,10 @@
 package it.unimib.adastra.ui.welcome;
 
+import static it.unimib.adastra.util.Constants.EMAIL_ADDRESS;
+import static it.unimib.adastra.util.Constants.ENCRYPTED_DATA_FILE_NAME;
+import static it.unimib.adastra.util.Constants.ENCRYPTED_SHARED_PREFERENCES_FILE_NAME;
+import static it.unimib.adastra.util.Constants.PASSWORD;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,14 +14,23 @@ import android.view.View;
 
 import org.apache.commons.validator.routines.EmailValidator;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 import it.unimib.adastra.R;
 import it.unimib.adastra.databinding.ActivityLoginBinding;
 import it.unimib.adastra.ui.main.MainActivity;
+import it.unimib.adastra.util.DataEncryptionUtil;
 
 public class Login extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
     String TAG = Login.class.getSimpleName();
+
+    private DataEncryptionUtil dataEncryptionUtil;
+
+    private String email;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +39,34 @@ public class Login extends AppCompatActivity {
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        dataEncryptionUtil = new DataEncryptionUtil(this);
+
+        try {
+            Log.d(TAG, "Email address from encrypted SharedPref: " + dataEncryptionUtil.
+                    readSecretDataWithEncryptedSharedPreferences(
+                            ENCRYPTED_SHARED_PREFERENCES_FILE_NAME, EMAIL_ADDRESS));
+            Log.d(TAG, "Password from encrypted SharedPref: " + dataEncryptionUtil.
+                    readSecretDataWithEncryptedSharedPreferences(
+                            ENCRYPTED_SHARED_PREFERENCES_FILE_NAME, PASSWORD));
+            Log.d(TAG, "Login data from encrypted file: " + dataEncryptionUtil.
+                    readSecretDataOnFile(ENCRYPTED_DATA_FILE_NAME));
+        } catch (GeneralSecurityException | IOException e) {
+            e.printStackTrace();
+        }
 
         binding.buttonLogin.setOnClickListener(v -> {
-            String email = binding.textEmail.getText().toString();
+            email = binding.textEmail.getText().toString();
             Log.d(TAG, "E-mail: " + email);
             Log.d(TAG, "E-mail: " + isEmailValid(email));
 
-            String password = binding.textPassword.getText().toString();
+            password = binding.textPassword.getText().toString();
             Log.d(TAG, "Password: " + password);
             Log.d(TAG, "Password: " + isPasswordValid(password));
 
             if(isEmailValid(email) && isPasswordValid(password)) {
-                Log.d(TAG, "Passagio a home page");
+                Log.d(TAG, "prima del salvataggio");
+                saveLoginData(email, password);
+                Log.d(TAG, "dopo il salvataggio");
                 Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
                 //finish();
@@ -71,5 +101,19 @@ public class Login extends AppCompatActivity {
         }
 
         return result;
+    }
+
+    private void saveLoginData(String email, String password) {
+        try {
+            dataEncryptionUtil.writeSecretDataWithEncryptedSharedPreferences(
+                    ENCRYPTED_SHARED_PREFERENCES_FILE_NAME, EMAIL_ADDRESS, email);
+            dataEncryptionUtil.writeSecretDataWithEncryptedSharedPreferences(
+                    ENCRYPTED_SHARED_PREFERENCES_FILE_NAME, PASSWORD, password);
+
+            dataEncryptionUtil.writeSecreteDataOnFile(ENCRYPTED_DATA_FILE_NAME,
+                    email.concat(":").concat(password));
+        } catch (GeneralSecurityException | IOException e) {
+            e.printStackTrace();
+        }
     }
 }
